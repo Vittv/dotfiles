dafa4f51b11dda24e83be5ff247e53543618abf7
Vittv <166668150+Vittv@users.noreply.github.com>
2025-11-25 17:47:00 -0300

    add rp and polish noice config

--- linux/.config/nvim/lua/plugins/noice.lua
+++ linux/.config/nvim/lua/plugins/noice.lua
@@ -7,7 +7,11 @@ return {
       hover = {
         border = {
           style = "rounded",
-        }
+        },
+        win_options = {
+          winblend = 0,
+          winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+        },
       }
     },
     lsp = {
@@ -22,6 +26,14 @@ return {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     "MunifTanjim/nui.nvim",
     -- OPTIONAL:
+    {
+      "rcarriga/nvim-notify",
+      config = function()
+        require("notify").setup({
+          background_colour = "#000000"
+        })
+      end
+    }
     --   `nvim-notify` is only needed, if you want to use the notification view.
     --   If not available, we use `mini` as the fallback
   },
