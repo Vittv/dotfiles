017b772a6cf8971bd263ddc97d4aa90bf0316d2f
Vittv <166668150+Vittv@users.noreply.github.com>
2026-01-16 03:53:29 -0300

    make powermenu minimal

--- scripts/.local/bin/powermenu.sh
+++ scripts/.local/bin/powermenu.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
+chosen=$(printf "󰐥\n󰍃\n󰌾\n󰤄\n󰜉\n󰒲" | rofi -dmenu -no-custom \
+    -theme-str 'mainbox {children: [listview];}' \
+    -theme-str 'window {width: 360px;}' \
+    -theme-str 'listview {columns: 3; lines: 2; spacing: 15px; fixed-columns: true;}' \
+    -theme-str 'element {padding: 22px; border-radius: 8px;}' \
+    -theme-str 'element-text {horizontal-align: 0.5; vertical-align: 0.5; padding: 0px;}' \
+    -theme-str 'configuration {show-icons: false; font: "AdwaitaMono Nerd Font Propo 28";}' \
+    -p "")
 
-chosen=$(printf "󰐥 Power Off\n󰜉 Restart\n󰌾 Lock\n󰍃 Logout\n󰤄 Suspend\n󰒲 Hibernate" | rofi -dmenu -no-custom -theme-str '@import "colors.rasi"' -theme-str 'mainbox {children: [listview];}' -theme-str 'window {width: 200px;}' -theme-str 'listview {lines: 6;}' -theme-str 'element-text {padding: 0px;}' -theme-str 'configuration {show-icons: false; font: "AdwaitaMono Nerd Font 14";}' -p "Power Menu")
 case "$chosen" in
-    "󰐥 Power Off") poweroff ;;
-    "󰜉 Restart") reboot ;;
-    "󰌾 Lock") hyprlock ;;
-    "󰍃 Logout") loginctl kill-session $XDG_SESSION_ID ;;
-    "󰤄 Suspend") systemctl suspend ;;
-    "󰒲 Hibernate") systemctl hibernate ;;
+    "󰐥") poweroff ;;
+    "󰜉") reboot ;;
+    "󰌾") hyprlock ;;
+    "󰍃") loginctl kill-session $XDG_SESSION_ID ;;
+    "󰤄") systemctl suspend ;;
+    "󰒲") systemctl hibernate ;;
     *) exit 1 ;;
 esac
+
+
