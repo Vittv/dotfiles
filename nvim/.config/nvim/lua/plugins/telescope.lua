ffc29a5a62ef373a028c08f82e0b9d8209e921eb
Vittv <166668150+Vittv@users.noreply.github.com>
2026-01-19 18:14:49 -0300

    prevent telescope from finding node_modules

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -13,6 +13,9 @@ return {
             require("telescope.themes").get_dropdown({}),
           },
         },
+        defaults = {
+          file_ignore_patterns = { "node_modules" }
+        }
       })
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
