65df158a521bfde1a1c5645792c07395e23c56ce
Vittv <166668150+Vittv@users.noreply.github.com>
2026-02-01 20:11:30 -0300

    add delta and fzf-git support for git diff

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -29,3 +29,27 @@ set fish_greeting
 
 # fzf keybinds
 fzf --fish | source
+
+# fzf-git shortcuts
+source ~/apps/fzf-git.sh/fzf-git.fish
+
+# FZF options
+set -x FZF_CTRL_T_OPTS "--preview 'bat -n --color=always --line-range :500 {}'"
+set -x FZF_ALT_C_OPTS "--preview 'eza --tree --color=always {} | head -200'"
+
+# Advanced customization function
+function _fzf_comprun
+  set command $argv[1]
+  set -e argv[1]
+  
+  switch $command
+    case cd
+        fzf --preview 'eza --tree --color=always {} | head -200' $argv
+    case export unset
+        fzf --preview "eval 'echo \$'{}" $argv
+    case ssh
+        fzf --preview 'dig {}' $argv
+    case '*'
+        fzf --preview "bat -n --color=always --line-range :500 {}" $argv
+  end
+end
