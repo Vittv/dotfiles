8c1e79654de4645ebd1b1900232f5459d757cea3
Vittv <166668150+Vittv@users.noreply.github.com>
2026-02-07 14:57:00 -0300

    rm font specification from script

--- scripts/.local/bin/powermenu.sh
+++ scripts/.local/bin/powermenu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-chosen=$(printf "󰐥 Power Off\n󰜉 Restart\n󰌾 Lock\n󰍃 Logout\n󰤄 Suspend\n󰒲 Hibernate" | rofi -dmenu -no-custom -theme-str 'mainbox {children: [listview];}' -theme-str 'window {width: 200px;}' -theme-str 'listview {lines: 6;}' -theme-str 'element-text {padding: 0px;}' -theme-str 'configuration {show-icons: false; font: "Iosevka Nerd Font Propo SemiBold 14";}' -p "Power Menu")
+chosen=$(printf "󰐥 Power Off\n󰜉 Restart\n󰌾 Lock\n󰍃 Logout\n󰤄 Suspend\n󰒲 Hibernate" | rofi -dmenu -no-custom -theme-str 'mainbox {children: [listview];}' -theme-str 'window {width: 200px;}' -theme-str 'listview {lines: 6;}' -theme-str 'element-text {padding: 0px;}' -theme-str 'configuration {show-icons: false;}' -p "Power Menu")
 
 case "$chosen" in
     "󰐥 Power Off") poweroff ;;
