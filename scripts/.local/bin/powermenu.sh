06cbcf39ee7a95d2cc2cb08aaf67cbf1bb30df77
Vittv <166668150+Vittv@users.noreply.github.com>
2026-02-08 05:02:28 -0300

    fix animation on powermenu

--- scripts/.local/bin/powermenu.sh
+++ scripts/.local/bin/powermenu.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-chosen=$(printf "󰐥   Power Off\n󰜉   Restart\n󰌾   Lock\n󰍃   Logout\n󰤄   Suspend\n󰒲   Hibernate" | rofi -dmenu -no-custom \
+chosen=$(printf "󰐥   Power Off\n󰜉   Restart\n󰌾   Lock\n󰍃   Logout\n󰤄   Suspend\n󰒲   Hibernate" | rofi -dmenu \
   -theme-str 'mainbox {children: [listview];}' \
-  -theme-str 'window {width: 200px; animation: "expand-vertical"; animation-duration: 300;}' \
+  -theme-str 'window {width: 200px;}' \
   -theme-str 'listview {lines: 6; dynamic: true; fixed-height: false;}' \
   -theme-str 'element-text {padding: 0px;}' \
   -theme-str 'configuration {show-icons: false;}' -p "Power Menu")
 
 case "$chosen" in
-    "󰐥 Power Off") poweroff ;;
-    "󰜉 Restart") reboot ;;
-    "󰌾 Lock") hyprlock ;;
-    "󰍃 Logout") loginctl kill-session $XDG_SESSION_ID ;;
-    "󰤄 Suspend") systemctl suspend ;;
-    "󰒲 Hibernate") systemctl hibernate ;;
+    "󰐥   Power Off") poweroff ;;
+    "󰜉   Restart") reboot ;;
+    "󰌾   Lock") hyprlock ;;
+    "󰍃   Logout") loginctl kill-session $XDG_SESSION_ID ;;
+    "󰤄   Suspend") systemctl suspend ;;
+    "󰒲   Hibernate") systemctl hibernate ;;
 esac
