2e6d4e334c95b29d7da62b6d3375c2cc5905e345
Vittv <166668150+Vittv@users.noreply.github.com>
2026-04-13 15:17:40 -0300

    polish image conf

--- nvim/.config/nvim/lua/plugins/image.lua
+++ nvim/.config/nvim/lua/plugins/image.lua
@@ -3,12 +3,16 @@ return {
   build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
   opts = {
     processor = "magick_cli",
-    markdown = {
-      enabled = true,
-      clear_in_insert_mode = false,
-      download_remote_images = true,
-      only_render_image_at_cursor = false,
-      filetypes = { "markdown", "vimwiki" },
-    },
+    integrations = {
+      markdown = {
+        enabled = true,
+        clear_in_insert_mode = false,
+        download_remote_images = true,
+        only_render_image_at_cursor = true,
+        only_render_image_at_cursor_mode = "popup",
+        floating_windows = true,
+        filetypes = { "markdown", "vimwiki" },
+      },
+    }
   },
 }
