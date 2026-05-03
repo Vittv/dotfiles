2012bccd5f6369350f0056995bd273008d0b1342
Vittv <166668150+Vittv@users.noreply.github.com>
2026-05-03 00:42:16 -0300

    rm delete param from ryujinx launcher

--- scripts/.local/bin/ryujinx-launcher.sh
+++ scripts/.local/bin/ryujinx-launcher.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 ~/build/ryujinx-1.3.3-x64.AppImage
 sleep 2
-rsync -a --delete ~/.config/Ryujinx/bis/user/save/ /mnt/drive/backup/games/01-saves/ryujinx-saves/
+rsync -a ~/.config/Ryujinx/bis/user/save/ /mnt/drive/backup/games/01-saves/ryujinx-saves/
 echo "Saves backed up!"
