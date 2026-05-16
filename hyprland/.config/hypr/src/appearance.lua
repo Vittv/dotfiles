ddafe13e893014113bcb0e7790cbe510c79dad01
Vittv <166668150+Vittv@users.noreply.github.com>
2026-05-16 05:16:43 -0300

    polish ui

--- hyprland/.config/hypr/src/appearance.lua
+++ hyprland/.config/hypr/src/appearance.lua
@@ -35,8 +35,8 @@ hl.config({
     },
     blur = {
       enabled           = true,
-      size              = 3,
-      passes            = 1,
+      size              = 4,
+      passes            = 2,
       new_optimizations = true,
       vibrancy          = 0.1696,
       popups            = true,
