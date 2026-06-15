29b7bcd7db35120891def6d771c37ab77fee459f
Vittv <166668150+Vittv@users.noreply.github.com>
2026-06-15 14:24:08 -0300

    style(sesh): use adaptive height and reverse layout in t picker

--- fish/.config/fish/functions/t.fish
+++ fish/.config/fish/functions/t.fish
@@ -1,3 +1,3 @@
 function t
-  sesh connect "$(sesh list --icons | fzf --no-sort --ansi --border --border-label " sesh " --prompt " " --header " ^t tmux ^z zoxide" --bind "ctrl-t:change-prompt( )+reload(sesh list -t --icons)" --bind "ctrl-z:change-prompt( )+reload(sesh list -z --icons)")"
+  sesh connect "$(sesh list --icons | fzf --no-sort --ansi --border --border-label " sesh " --prompt " " --header " ^t tmux ^z zoxide" --height ~40% --layout reverse --bind "ctrl-t:change-prompt( )+reload(sesh list -t --icons)" --bind "ctrl-z:change-prompt( )+reload(sesh list -z --icons)")"
 end
