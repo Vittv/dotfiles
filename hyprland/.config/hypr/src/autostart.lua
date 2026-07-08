b9465a39b76844997a79cbeba1a037cec4bf83c0
Vittv <166668150+Vittv@users.noreply.github.com>
2026-07-08 04:48:03 -0300

    hypr(apps): add dunst autostart, and restore rofi as menu

--- hyprland/.config/hypr/src/autostart.lua
+++ hyprland/.config/hypr/src/autostart.lua
@@ -4,9 +4,9 @@
 
 hl.on("hyprland.start", function()
   hl.exec_cmd("waybar")
+  hl.exec_cmd("dunst")
   hl.exec_cmd("hyprpaper")
   hl.exec_cmd("hyprsunset")
-  hl.exec_cmd("swaync")
   hl.exec_cmd("/usr/lib/hyprpolkitagent/hyprpolkitagent")
   hl.exec_cmd("/usr/lib/xdg-desktop-portal-hyprland")
   hl.exec_cmd("gnome-keyring-daemon --start --components=secrets")
