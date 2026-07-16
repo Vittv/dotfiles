e24c9accbc47ec62879e6b77a91ae73de654873f
Vittv <166668150+Vittv@users.noreply.github.com>
2026-07-16 00:43:04 -0300

    fix(hypr): switch to qs launcher, add persistent ws

--- hyprland/.config/hypr/src/workspaces.lua
+++ hyprland/.config/hypr/src/workspaces.lua
@@ -3,11 +3,11 @@
 -- ╚══════════════════════════════════╝
 
 -- monitor 1
-hl.workspace_rule({ workspace = "1", monitor = "DP-1" })
-hl.workspace_rule({ workspace = "2", monitor = "DP-1" })
-hl.workspace_rule({ workspace = "3", monitor = "DP-1" })
-hl.workspace_rule({ workspace = "4", monitor = "DP-1" })
+hl.workspace_rule({ workspace = "1", monitor = "DP-1", persistent = true })
+hl.workspace_rule({ workspace = "2", monitor = "DP-1", persistent = true })
+hl.workspace_rule({ workspace = "3", monitor = "DP-1", persistent = true })
+hl.workspace_rule({ workspace = "4", monitor = "DP-1", persistent = true })
 
 -- monitor 2
-hl.workspace_rule({ workspace = "5", monitor = "HDMI-A-1" })
-hl.workspace_rule({ workspace = "6", monitor = "HDMI-A-1" })
+hl.workspace_rule({ workspace = "5", monitor = "HDMI-A-1", persistent = true })
+hl.workspace_rule({ workspace = "6", monitor = "HDMI-A-1", persistent = true })
