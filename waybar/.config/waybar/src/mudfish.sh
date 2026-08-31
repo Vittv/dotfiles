605bc20423ac6bf683af010d14bd1331807bc084
Vittv <166668150+Vittv@users.noreply.github.com>
2026-08-31 05:45:55 -0300

    polish waybar

--- waybar/.config/waybar/src/mudfish.sh
+++ waybar/.config/waybar/src/mudfish.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 if pgrep -u root mudrun-headless > /dev/null 2>&1; then
-    text="<span color='#7fa563'>▪</span> mud"
+    text="<span color='#7fa563'>▪</span>󰱓"
     tip="Mudfish VPN is ON"
     cls="on"
 else
-    text="<span color='#d8647e'>▪</span> mud"
+    text="<span color='#d8647e'>▪</span>󰅛"
     tip="Mudfish VPN is OFF"
     cls="off"
 fi
